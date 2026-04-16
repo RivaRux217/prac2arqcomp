30f5ba4d18a31495e51e7f6fac8de912b51d83c7
RivaRux217 <58639413+RivaRux217@users.noreply.github.com>
2026-04-16 11:57:21 +0200

    Update job time in compilarv2.sh
    Reduced job time from 3 hours to 30 minutes in SLURM script.

--- compilarv2.sh
+++ compilarv2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Solicitamos un nodo con 64 cores y 16 GB de memoria durante 15 minutos
 # Cambiar no sbatch para menos tempo e memoria para que nos den acceso máis fácilmente. Facer unha prueba con pouco
-#SBATCH -n 1 -c 64 -t 03:00:00 --mem=16G
+#SBATCH -n 1 -c 64 -t 00:30:00 --mem=16G
 # Ponemos nombre a nuestro trabajo para poder identificarlo.
 # ATENCIÓN - Debes sustituir el NN por el número de equipo.
 #SBATCH --job-name p1acg1013
